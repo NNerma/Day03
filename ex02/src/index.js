32017ac97b3d11104d494ddb31132d4baa079843
NNerma <nnerma.suljevic@gmail.com>
2021-03-15 10:59:02 +0100

    Day03

---
+++ ex02/src/index.js
@@ -0,0 +1,18 @@
+function main() {
+    // Only change code below this line
+
+    //this is a single line JS comment.
+
+    /*  this is multi-line comment!
+        this is a multi-line comment!
+        this is a multi line comment!
+        this is a multi-line comment!
+        this is a multi-line cimment!
+    */
+
+    // Only change code above this line
+}
+
+console.log(main());
+
+module.exports = main;
---
+++ ex10/src/index.js
@@ -0,0 +1,15 @@
+function main(numOne, numTwo) {
+    // Only change code below this line
+    
+    var product=numOne * numTwo;
+    var numOne;
+    var numTwo;
+    
+
+    // Only change code above this line
+    return product;
+}
+
+console.log(main(15, 3));
+
+module.exports = main;
